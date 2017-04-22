c80eeeacda9f8896342e1010f06a8e72231adec7
Tim Reinagel <tmr2_handbell@yahoo.com>
2017-04-21 21:50:02 -0700

    Statement navigation added.

--- AllPlanet/Argument/ArgumentPoint.cs
+++ AllPlanet/Argument/ArgumentPoint.cs
@@ -1,14 +1,38 @@
 ﻿using AllPlanet.Refute;
+using System;
 
 namespace AllPlanet.Argument
 {
     public class ArgumentPoint
     {
         public Statement[] Statements;
+        public Statement CurrentStatement;
+        public bool HasNext => indexer < Statements.Length - 1;
+        public bool HasPrior => indexer > 0;
+        private int indexer;
 
-        public ArgumentPoint(Statement[] statement)
+        public ArgumentPoint(Statement[] statements)
         {
-            Statements = statement;
+            Statements = statements;
+            CurrentStatement = Statements[0];
+            indexer = 0;
+        }
+
+        public void Next()
+        {
+            indexer = Math.Max(indexer + 1, Statements.Length - 1);
+            CurrentStatement = Statements[indexer];
+        }
+
+        public void Prior()
+        {
+            indexer = Math.Min(indexer - 1, 0);
+            CurrentStatement = Statements[indexer];
+        }
+
+        public void Reset()
+        {
+            CurrentStatement = Statements[0];
         }
     }
 }
