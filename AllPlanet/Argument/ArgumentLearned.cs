88c277efab56993a89fb6070d53fd583b6b376e0
Tim Reinagel <tmr2_handbell@yahoo.com>
2017-04-22 13:58:14 -0700

    ArgumentLearned event added.

---
+++ AllPlanet/Argument/ArgumentLearned.cs
@@ -0,0 +1,14 @@
+﻿namespace AllPlanet.Argument
+{
+    public class ArgumentLearned
+    {
+        public ArgumentType Type;
+        public string Description;
+
+        public ArgumentLearned(ArgumentType type, string description)
+        {
+            Type = type;
+            Description = description;
+        }
+    }
+}
