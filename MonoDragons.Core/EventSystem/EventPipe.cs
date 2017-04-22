f2671871ca0787aa0605f708c28cee45af916ae4
Tim Reinagel <tmr2_handbell@yahoo.com>
2017-04-22 01:52:55 -0700

    Improved event pipeline.

---
+++ MonoDragons.Core/EventSystem/EventPipe.cs
@@ -0,0 +1,27 @@
+﻿using MonoDragons.Core.Engine;
+using System;
+using System.Collections.Generic;
+
+namespace MonoDragons.Core.EventSystem
+{
+    public class EventPipe
+    {
+        private List<Action> actions = new List<Action>();
+        public bool HasNext => actions.Count > 0;
+        
+        public EventPipe()
+        {
+        }
+
+        public void Subscribe<T>(Action<T> act)
+        {
+            World.Subscribe(EventSubscription.Create<T>((e) => actions.Add(() => act(e)), this));
+        }
+
+        public void Dequeue()
+        {
+            actions[0]();
+            actions.RemoveAt(0);
+        }
+    }
+}
