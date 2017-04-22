686fa24626cedcde834c9cef71d907cf130490c1
Tim Reinagel <tmr2_handbell@yahoo.com>
2017-04-21 21:03:04 -0700

    Create save quotes complete.

--- AllPlanet/Argument/Statement.cs
+++ AllPlanet/Argument/Statement.cs
@@ -7,16 +7,20 @@ namespace AllPlanet.Argument
     public class Statement
     {
         private readonly List<RefuteOption> _options;
+        private readonly Dictionary<string, RefuteOption> _discredits;
+        private readonly RefuteOption _genericDiscredit;
 
         public string Message { get; }
         public OpponentExpression Expression { get; }
         public List<string> Options => _options.Select(c => c.Name).ToList();
         public bool IsRefutable => Options.Count > 0;
 
-        public Statement(string message, List<RefuteOption> options)
+        public Statement(string message, List<RefuteOption> options, Dictionary<string, RefuteOption> discredits, RefuteOption genericDiscredit)
         {
             Message = message;
             _options = options;
+            _discredits = discredits;
+            _genericDiscredit = genericDiscredit;
         }
 
         public void Counter(string optionName)
@@ -24,6 +28,18 @@ namespace AllPlanet.Argument
             var counter = _options.Find(c => c.Name == optionName);
             counter.Choose();
             _options.Remove(counter);
+        }
+
+        public void Discredit(string statement)
+        {
+            if(_discredits.ContainsKey(statement))
+            {
+                var discredit = _discredits[statement];
+                _discredits.Remove(statement);
+                discredit.Choose();
+            }
+            else
+                _genericDiscredit.Choose();
         }
     }
 }
