02d66a6ef5bd2fb317820cd852ac17c0e8237cb1
Nate Fuller <fuller.nate@gmail.com>
2017-04-22 20:40:56 -0700

    keyboard bindings...

--- AllPlanet/Argument/ArgumentNavUI.cs
+++ AllPlanet/Argument/ArgumentNavUI.cs
@@ -13,22 +13,19 @@ namespace AllPlanet.Argument
 
         private readonly CurrentPoint _currentPoint;
 
-        private bool HasNext => _currentPoint.Get().HasNext;
-        private bool HasBack => _currentPoint.Get().HasPrior;
-
         public ClickUIBranch Branch { get; }
 
         public ArgumentNavUI(CurrentPoint point)
         {
             _currentPoint = point;
             Branch = new ClickUIBranch("Nav", (int)ClickBranchPriority.Navigation);
-            _backButton = Buttons.CreateBack(new Transform2(new Vector2(100, 400), new Size2(64, 64)), Back, () => HasBack);
-            _nextButton = Buttons.CreateNext(new Transform2(new Vector2(1500, 400), new Size2(64, 64)), Next, () => HasNext);
-            Branch.Add(_nextButton);
+            _backButton = Buttons.CreateBack(new Transform2(new Vector2(100, 400), new Size2(64, 64)), Back, HasBack);
+            _nextButton = Buttons.CreateNext(new Transform2(new Vector2(1500, 400), new Size2(64, 64)), Next, HasNext);
             Branch.Add(_backButton);
+            Branch.Add(_nextButton);
 
-            Input.On(Control.Right, () => { Next(); });
-            Input.On(Control.Left, () => { Back(); });
+            Input.On(Control.Right, Next);
+            Input.On(Control.Left, Back);
         }
 
         public void Draw(Transform2 parentTransform)
@@ -37,14 +34,18 @@ namespace AllPlanet.Argument
             _backButton.Draw(parentTransform);
         }
 
+        private bool HasNext() => _currentPoint.Get().HasNext;
         private void Next()
         {
-            _currentPoint.Get().Next();
+            if (HasNext())
+                _currentPoint.Get().Next();
         }
 
+        private bool HasBack() => _currentPoint.Get().HasPrior;
         private void Back()
         {
-            _currentPoint.Get().Prior();
+            if (HasBack())
+                _currentPoint.Get().Prior();
         }
     }
 }
