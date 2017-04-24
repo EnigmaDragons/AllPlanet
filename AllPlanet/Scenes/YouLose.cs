fffae9af9e0af032985eb293e333074edea56104
Silas Reinagel <silas.reinagel@gmail.com>
2017-04-24 02:30:53 -0700

    Improved GameOver explosion animation. Closes #89

--- AllPlanet/Scenes/YouLose.cs
+++ AllPlanet/Scenes/YouLose.cs
@@ -11,21 +11,33 @@ namespace AllPlanet.Scenes
     public class YouLose : IScene
     {
         private ClickUI _clickUi;
-        private Timer _animTimer;
+        private Timer _expTimer;
+        private Timer _seqTimer;
         private ImageTextButton _continue;
 
         private int _phase = 0;
         private bool _planetAlive = true;
         private bool _isExploding;
+        private int _explodingFrame = 1;
 
         public void Init()
         {
             _clickUi = new ClickUI();
             _continue = Buttons.CreateContinue(new Transform2(new Vector2(700, 550), new Size2(200, 60)), () => World.NavigateToScene("Credits"));
             _continue.IsEnabled = false;
-            _animTimer = new Timer(Next, 500);
+            _expTimer = new Timer(UpdateExpFrame, 90);
+            _seqTimer = new Timer(Next, 500);
             _clickUi.Add(_continue);
             Audio.PlayMusicOnce("Music/gameover");
+        }
+
+        private void UpdateExpFrame()
+        {
+            if (!_isExploding)
+                return;
+            _explodingFrame++;
+            if (_explodingFrame > 8)
+                _isExploding = false;
         }
 
         private void Next()
@@ -38,17 +50,15 @@ namespace AllPlanet.Scenes
             }
             if (_phase == 5)
                 _planetAlive = false;
-            if (_phase == 8)
-            {
-                _isExploding = false;
+            if (_phase == 10)
                 _continue.IsEnabled = true;
-            }
         }
 
         public void Update(TimeSpan delta)
         {
+            _expTimer.Update(delta);
             _clickUi.Update(delta);
-            _animTimer.Update(delta);
+            _seqTimer.Update(delta);
         }
 
         public void Draw()
@@ -58,7 +68,7 @@ namespace AllPlanet.Scenes
             if (_planetAlive)
                 UI.DrawCenteredWithOffset("Characters/planet-sad", new Vector2(600, 300), new Vector2(0, 150));
             if (_isExploding)
-                UI.DrawCenteredWithOffset("Anim/explosion4", new Vector2(550, 450) * 1.3f, new Vector2(0, 150));
+                UI.DrawCenteredWithOffset($"Anim/explosion{_explodingFrame}", new Vector2(550, 450) * 1.3f, new Vector2(0, 150));
             if (!_planetAlive && !_isExploding)
                 _continue.Draw(Transform2.Zero);
         }
