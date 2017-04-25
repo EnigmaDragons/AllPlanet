3df4fa63a0f489315554900626705b439e06bad5
noah reinagel <noah.r9@gmail.com>
2017-04-24 17:28:26 -0700

    fixed bug

--- AllPlanet/Argument/Concrete/QuintonRocheford/GravityIsTooWeakArgument.cs
+++ AllPlanet/Argument/Concrete/QuintonRocheford/GravityIsTooWeakArgument.cs
@@ -14,7 +14,6 @@ namespace AllPlanet.Argument.Concrete
                 new PlanetResponds("*What does that mean?*", PlanetExpression.Thinking),
                 new OpponentResponds("I am able to stand on this podium without so much as a pull from your gravity. You're gravity is practically non-existent!", OpponentExpression.Proud),
                 new OpponentResponds("That's the reason you don't have a moon.", OpponentExpression.Challenging),
-                new OpponentResponds("You're gravity is practically non-existent.", OpponentExpression.Proud),
                 new OpponentResponds("Planet's are formed by gravity pulling things together.", OpponentExpression.Challenging),
                 new OpponentResponds("With no gravity like that, you are not a planet.", OpponentExpression.Challenging),
                 new CrowdResponds(CrowdExpression.NoComment),
