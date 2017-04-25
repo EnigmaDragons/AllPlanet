064871f5011ca60c6a12b78b58da56df3755f4ab
noah reinagel <noah.r9@gmail.com>
2017-04-24 17:20:00 -0700

    Corrected naming of transition

--- AllPlanet/Transitions/Concrete/PreClosingArgumentWithEmerson.cs
+++ AllPlanet/Transitions/Concrete/PreClosingArgumentWithEmerson.cs
@@ -5,7 +5,7 @@ namespace AllPlanet.Transitions.Concrete
 {
     public static class PreClosingArgumentWithEmerson
     {
-        public static Transition Transition { get; } = new Transition("emerson closing",
+        public static Transition Transition { get; } = new Transition("emerson rebutal",
             new PlanetResponds("*I gotta give him an argument of my own*", PlanetExpression.Thinking),
             new PlanetResponds("*I am going to choose carefully how to build my argument from my choices*", PlanetExpression.Thinking),
             new Segue("emerson closing"));
